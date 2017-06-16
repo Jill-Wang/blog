3b0f9f5ed2cad54d667112560e2251837e5f2325
wangjing0612 <wangjing0612@autohome.com.cn>
2017-06-16 15:11:29 +0800

    abc

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,3 +1,4 @@
+#encoding:utf-8
 from __future__ import absolute_import, unicode_literals
 
 # 这将保证celery app总能在django应用启动时启动
--- blog/celery.py
+++ blog/celery.py
@@ -1,3 +1,4 @@
+#encoding:utf-8
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
